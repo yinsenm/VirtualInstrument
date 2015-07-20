baa3129793046b8de3444eb070ff04a6de8c05b4
= <=>
2015-07-20 17:33:08 +0800

    07/20

--- slave/main.c
+++ slave/main.c
@@ -8,7 +8,7 @@
 
 unsigned char ex0_flag = 0; //外部中断INT0的中断标志，2次中断后开始计时
 unsigned char cal_flag = 0; //计算标志位，标志为1时开始计算
-unsigned short count = 0;
+unsigned int count = 0;
 
 void EX0_Init() {
 	EA = 1;
@@ -17,6 +17,8 @@ void EX0_Init() {
 	TMOD |= 0x01;
 	TH0 = 0;
 	TL0 = 0;
+	EX0 = 1;
+	ET0 = 1;
 }
 
 void EX0_ISR () interrupt 0 {
@@ -35,14 +37,14 @@ void EX0_ISR () interrupt 0 {
 }
 
 void Timer0_ISR() interrupt 1{
-	count ++;
+	count = count + 1;
 	TL0 = 0;
 	TH0 = 0;
 }
 
 void main() {
 	unsigned int adc1, adc2;
-	unsigned long rotation;
+	//unsigned long rotation;
 	UART_Init();
 	TM7705_Init();
 	EX0_Init();
@@ -53,18 +55,19 @@ void main() {
 		adc1 = TM7705_ReadAdc(1);
 		adc2 = TM7705_ReadAdc(2);
 		
-		EX0 = 1;
 		//while(cal_flag != 1);
 		if (cal_flag == 1) {
 			EX0 = 0;
 			cal_flag = 0;
+			//rotation = 65536 * count + 256 * TH0 + TL0;
+			printf("%d \r\n", count);
+			printf("%8.3f Hz \r\n", 1000000/(1.08*(65536 * count + 256 * TH0 + TL0)));
 			count = 0;
-			rotation = 65536 * count + 256 * TH0 + TL0;
-			printf("rotation = (%10.5f) s \n", (1.08*rotation)/1000000);
-		} else {
-			printf("not detected\r\n");
-		}
-		printf("CH1 = %04X, (%7.5f) V \t CH2 = %04X, (%7.5f) V \r\n", adc1, (adc1*5.0)/65535, adc2, (adc2*5.0)/65535);
+			TH0 = 0;
+			TL0 = 0;
+			EX0 = 1;
+		} 
+		//printf("CH1 = %04X, (%7.5f) V \t CH2 = %04X, (%7.5f) V \r\n", adc1, (adc1*5.0)/65535, adc2, (adc2*5.0)/65535);
 		//printf("%d \t %X \t %X \r\n", count, TH0, TL0);
 
 	}
