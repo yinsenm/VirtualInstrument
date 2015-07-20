a74bdccb67045d870d8e27a7279dbc682620f63e
Yinsen Miao <yinsenm@gmail.com>
2015-07-20 11:45:13 +0800

    7/19

--- slave/main.c
+++ slave/main.c
@@ -6,9 +6,9 @@
 #include <stdio.h>
 #include <reg52.h>
 
-unsigned char flag = 0;
+unsigned char ex0_flag = 0; //外部中断INT0的中断标志，2次中断后开始计时
+unsigned char cal_flag = 0; //计算标志位，标志为1时开始计算
 unsigned short count = 0;
-unsigned long rotation = 0;
 
 void EX0_Init() {
 	EA = 1;
@@ -20,17 +20,18 @@ void EX0_Init() {
 }
 
 void EX0_ISR () interrupt 0 {
-	if (flag == 0) {
-		TR0 = 1;
-		flag = 1;
+	ex0_flag++;
+	if(ex0_flag == 2) {
+		TR0 =~ TR0; //开启关闭定时器，第二次触发开，第三次触发关
+		if(TR0) { //定时器开时
+			ex0_flag = 1;
+		}
+		if(!TR0) {        
+			EX0 = 0;      
+			cal_flag = 1; //计算标志置1，即可以计算两次下降沿的值			
+			ex0_flag = 0;
+		}
 	} 
-
-	else if (flag == 1) {
-		TR0 = 0;
-		EX0 = 0;
-		flag = 2;
-	}
-	
 }
 
 void Timer0_ISR() interrupt 1{
@@ -41,28 +42,30 @@ void Timer0_ISR() interrupt 1{
 
 void main() {
 	unsigned int adc1, adc2;
+	unsigned long rotation;
 	UART_Init();
 	TM7705_Init();
 	EX0_Init();
 	//自校准
 	TM7705_CalibSelf(1);
 	TM7705_CalibSelf(2);
-	while(1){
+	while(1) {
 		adc1 = TM7705_ReadAdc(1);
 		adc2 = TM7705_ReadAdc(2);
+		
 		EX0 = 1;
-
-		while(flag != 2);
-		if (flag == 2) {
+		//while(cal_flag != 1);
+		if (cal_flag == 1) {
 			EX0 = 0;
-			flag = 0;
+			cal_flag = 0;
 			count = 0;
 			rotation = 65536 * count + 256 * TH0 + TL0;
+			printf("rotation = (%10.5f) s \n", (1.08*rotation)/1000000);
+		} else {
+			printf("not detected\r\n");
 		}
+		printf("CH1 = %04X, (%7.5f) V \t CH2 = %04X, (%7.5f) V \r\n", adc1, (adc1*5.0)/65535, adc2, (adc2*5.0)/65535);
+		//printf("%d \t %X \t %X \r\n", count, TH0, TL0);
 
-		//printf("CH1 = %04X, (%7.5f) V \t CH2 = %04X, (%7.5f) V \r\n", adc1, (adc1*5.0)/65535, adc2, (adc2*5.0)/65535);
-		printf("%d \t %X \t %X \r\n", count, TH0, TL0);
-		printf("rotation = %10.5f Hz \n", 1000000/(rotation * 0.9216));
 	}
 }
-
